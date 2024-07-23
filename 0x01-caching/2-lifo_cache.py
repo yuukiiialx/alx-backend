0707d06786acd51dabfcd22f8ccc70cbc2889e8f
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-23 06:10:24 +0300

    Update 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+LIFOCache module
+"""
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFOCache class"""
+
+    def __init__(self):
+        super().__init__()
+        self._last_key = None
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            if (
+                len(self.cache_data) == self.MAX_ITEMS
+                and key not in self.cache_data.keys()
+            ):
+                discarded_key = self._last_key
+                del self.cache_data[discarded_key]
+                print(f"DISCARD: {discarded_key}")
+
+            self.cache_data[key] = item
+            self._last_key = key
+
+    def get(self, key):
+        """Get an item by key"""
+        return self.cache_data.get(key, None)
