0ed649f6576fc96f1f221fe400fbb18849cc8c6f
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-23 05:43:11 +0300

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+0. Simple helper function
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Return a tuple of size two containing a start index and an end index
+    """
+    next_page: int = page * page_size
+    prev_page: int = next_page - page_size
+    return (prev_page, next_page)
