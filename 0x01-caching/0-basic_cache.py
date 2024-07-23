c1234edc295069a94aa93ec3066eb3b500930760
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-23 06:09:51 +0300

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+BasicCache module
+"""
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache class"""
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        return self.cache_data.get(key, None)
