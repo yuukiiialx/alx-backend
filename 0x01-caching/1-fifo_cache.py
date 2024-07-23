408d7001188fae894fb0ae973d3be53051347799
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-23 06:10:02 +0300

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+FIFOCache module
+"""
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFOCache class"""
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > self.MAX_ITEMS:
+                discarded_key = sorted(self.cache_data.keys())[0]
+                del self.cache_data[discarded_key]
+                print(f"DISCARD: {discarded_key}")
+
+    def get(self, key):
+        """Get an item by key"""
+        return self.cache_data.get(key, None)
