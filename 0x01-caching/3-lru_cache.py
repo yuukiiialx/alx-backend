c0241a1a24e4a1b34d65b6dfa4593c3154fcf260
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-23 06:10:38 +0300

    Update 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""
+LRUCache module
+"""
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRUCache class"""
+
+    def __init__(self):
+        super().__init__()
+        self._lru_keys = []
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key in self._lru_keys:
+                self._lru_keys.remove(key)
+            self._lru_keys.append(key)
+            if len(self.cache_data) > self.MAX_ITEMS:
+                removed_key = self._lru_keys.pop(0)
+                del self.cache_data[removed_key]
+                print(f"DISCARD: {removed_key}")
+
+    def get(self, key):
+        """Get an item by key"""
+        if key in self.cache_data.keys():
+            self._lru_keys.pop(self._lru_keys.index(key))
+            self._lru_keys.append(key)
+        return self.cache_data.get(key, None)
