bc09ec7ea4d22cc97105415216809daf444d7631
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-30 22:23:44 +0300

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,1 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+0-app.py
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world() -> str:
+    """Return a simple string as our response"""
+    return render_template("0-index.html")
