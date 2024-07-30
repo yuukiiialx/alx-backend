628def3f308caad92f3ec3b87e4f9a1a7213e265
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-30 22:24:48 +0300

    Update 4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,1 +1,37 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Defines flask app"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration class"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Function to get the locale"""
+    locale_param = request.args.get("locale")
+    if locale_param and locale_param in app.config["LANGUAGES"]:
+        return locale_param
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/", strict_slashes=False)
+def index() -> str:
+    """Render 3-index.html"""
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
